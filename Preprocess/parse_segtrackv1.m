42723765f3b0ff75e36e5f336d3aaea0e49ad434
chenzy <chenzeyuczy@qq.com>
2017-09-19 17:04:49 +0800

    Purify codes in preprocess.

--- Preprocess/parse_segtrackv1.m
+++ Preprocess/parse_segtrackv1.m
@@ -1,48 +1,49 @@
-% Explanation:
-% Extract info of SegTrackV2: according to txt files, do not use all the frames.
+% Extract data from SegTrackV1.
 
 % Input:
 % segtrack_root=[pwd '/Dataset/SegTrackv1/'];
 
+% Output:
+% dataset_info: A cell with raw images and ground-truth.
 
 
 function [dataset_info] = parse_segtrackv1(segtrack_root)
 
 	video_name = dir(segtrack_root);
-	video_name(1:2) = [];
     % Remove first two elements from array.
+	video_name(1:2) = [];
     dataset_info = cell(length(video_name), 1);
 
 	for i = 1:length(video_name)
 		dataset_info{i}.data_name = video_name(i).name;
 		video_root = [segtrack_root video_name(i).name '/'];
 		video_info = dir(video_root);
-		video_info(1:2) = [];
         % Remove first two elements from array.
+		video_info(1:2) = [];
         
+        % Remove ground-truth from array.
 		for j = 1:length(video_info)
-			if strcmp(video_info(j).name,'ground-truth') == 1
+            % Find index of ground-truth.
+			if strcmp(video_info(j).name, 'ground-truth') == 1
 				delete_flag = j;
-                % Find index of ground-truth.
 				break;
 			end
 		end 
 		video_info(delete_flag) = [];
-        % Remove ground-truth from array.
         
 		for j = 1:length(video_info)
 			img_path = [video_root video_info(j).name];
-			dataset_info{i}.data{j,1} = imread(img_path);
-			dataset_info{i}.img_name{j,1} = video_info(j).name;
+			dataset_info{i}.data{j, 1} = imread(img_path);
+			dataset_info{i}.img_name{j, 1} = video_info(j).name;
 		end
 
 		gt_info = dir([video_root 'ground-truth/']);
 		gt_info(1:2) = [];
 		for j = 1:length(gt_info)
 			gt_path = [video_root 'ground-truth/' gt_info(j).name];
-			dataset_info{i}.gt_name{j,1} = gt_info(j).name;
-			dataset_info{i}.gt{j,1} = imread(gt_path);
-			dataset_info{i}.gt{j,1} = im2bw(dataset_info{i}.gt{j});
+			dataset_info{i}.gt_name{j, 1} = gt_info(j).name;
+			dataset_info{i}.gt{j, 1} = imread(gt_path);
+			dataset_info{i}.gt{j, 1} = im2bw(dataset_info{i}.gt{j});
 		end
 	end
 
--- Preprocess/parse_segtrackv2.m
+++ Preprocess/parse_segtrackv2.m
@@ -1,5 +1,4 @@
-% Explanation:
-% Extract info of SegTrackV2: according to txt files, do not use all the frames.
+% Extract data from SegTrackV2.
 
 % Input:
 % segtrack_root: [pwd '/Dataset/SegTrackv2/']
@@ -16,45 +15,45 @@ function [dataset_info] = parse_segtrackv2(segtrack_root)
     % Remove asterisk(*) at the front.
 	datasets_all = cellfun(@(str) str(2:end), datasets_all, 'UniformOutput',  false);
 	
-    multi_object_ind = [4 6 7 9 10 12];    % delete videos that have multi-objects
-	for i=1:length(multi_object_ind)
-		datasets_all(multi_object_ind(i)-i+1) = [];   
+    multi_object_ind = [4 6 7 9 10 12];    % delete videos with multi-objects
+	for i = 1:length(multi_object_ind)
+		datasets_all(multi_object_ind(i) - i + 1) = [];   
 	end
 
 	dataset_info = cell(length(datasets_all), 1);   
 
-	for i = 1:length(datasets_all)    % process 14 sub_datasets in SegTrackV2
+	for i = 1:length(datasets_all)  % process 14 sub_datasets in SegTrackV2
+		dataset_info{i}.data_name = datasets_all{i};    % store sub_dataset name
 		
+		% Load image names.
 		img_names = textread([segtrack_root 'ImageSets/' datasets_all{i} '.txt'],  '%s' );
-		dataset_info{i}.img_path = cell(length(img_names)-1, 1);
-		dataset_info{i}.img_name = cell(length(img_names)-1, 1);
+		dataset_info{i}.img_path = cell(length(img_names) - 1, 1);
+		dataset_info{i}.img_name = cell(length(img_names) - 1, 1);
 		img_folder = [segtrack_root 'JPEGImages/' datasets_all{i} '/'];
 		files = dir(img_folder);
-		%[~, ~, ext] = fileparts(files(3).name); 	% confirm .PNG or .BMP format in SegTrackV2
-		[~, ~, ext] = fileparts(files(4).name); 	% Avoid confusion caused by mat files.
-		for j = 2:length(img_names)
-			dataset_info{i}.img_path{j-1} = [img_folder img_names{j}  ext];   % (j-1) is to delete the folder name in the 1st line in txt
+		[~, ~, ext] = fileparts(files(4).name);  % Avoid confusion extension caused by mat files.
+		for j = 2:length(img_names)  % (j-1) is intended to skip folder name in first line of the txt
+			dataset_info{i}.img_path{j-1} = [img_folder img_names{j} ext];
 			dataset_info{i}.img_name{j-1} = img_names{j};
 		end
 
-		dataset_info{i}.data = cell(length(dataset_info{i}.img_path), 1);    % read img data in each sub_dataset
+		% Load image data.
+		dataset_info{i}.data = cell(length(dataset_info{i}.img_path), 1);
         for k = 1:length(dataset_info{i}.img_path)    
             dataset_info{i}.data{k} = imread(dataset_info{i}.img_path{k});
         end
 
-		dataset_info{i}.data_name = datasets_all{i};    % store sub_dataset name
-
-		gt_folder=[segtrack_root 'GroundTruth/' dataset_info{i}.data_name];
-	    temp = dir([gt_folder '/*.*']);
-	    [~,~, gt_format] = fileparts( temp(3).name);
+        % Load ground truth.
+		gt_folder = [segtrack_root 'GroundTruth/' dataset_info{i}.data_name];
+	    gt_files = dir([gt_folder '/*.*']);
+	    [~, ~, gt_format] = fileparts(gt_files(3).name);  % Skip first 2 items returned by dir.
 	    gt_format = gt_format(2:end);
 	    dataset_info{i}.gt = cell(length(dataset_info{i}.data),1);
 	    for j = 1:length(dataset_info{i}.data)
-	        color_gt = imread( [gt_folder '/' dataset_info{i}.img_name{j} '.' gt_format] );
-            dataset_info{i}.gt{j}=im2bw(color_gt);
+	        color_gt = imread([gt_folder '/' dataset_info{i}.img_name{j} '.' gt_format]);
+            dataset_info{i}.gt{j} = im2bw(color_gt);
 	    end
 
 	end
 
 end
-
